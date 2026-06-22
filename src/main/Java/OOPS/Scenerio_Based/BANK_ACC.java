0b43b68481c2f09f370aba7121b4550a8e33d6eb
Ritika Soni <soniritika172006@gmail.com>
2026-06-22 13:02:26 +0530

    completed oops scenerio based day 9 problem

--- src/main/Java/OOPS/Scenerio_Based/BANK_ACC.java
+++ src/main/Java/OOPS/Scenerio_Based/BANK_ACC.java
@@ -12,3 +12,50 @@ class BankAccount {
         this.balance = balance;
         totalAccounts++;
     }
+    public void deposit(int amt){
+        if(balance>0){
+            balance+=amt;
+            System.out.println("Deposited");
+        }
+    }
+    public void withdraw("int amt"){
+        if(balance >0){
+            balance-=amt;
+            System.out.println("withdrawl completed");
+        }
+    }
+    public void getStatement(String accountNumber, String holder, double balance){
+        System.out.println("Account number:"+accountNumber);
+        System.out.println("Holder Name:"+holder);
+        System.out.println("Total Balance"+balance);
+    }
+}
+public class Main {
+    public static void main{
+        BankAccount acc1=new BankAccount("101","Ritika",5000);
+        BankAccount acc2=new BankAccount("102","Aman",8000);
+        BankAccount acc3=new BankAccount("103","Joe",3000);
+
+        acc1.deposit(1000);
+        acc1.withdraw(100);
+        acc1.withdraw(20);
+        acc1.deposit(1000);
+        acc1.deposit(500);
+
+        acc2.deposit(1000);
+        acc2.withdraw(100);
+        acc2.withdraw(20);
+        acc2.deposit(1000);
+        acc2.deposit(500);
+
+        acc3.deposit(1000);
+        acc3.withdraw(100);
+        acc3.withdraw(20);
+        acc3.deposit(1000);
+        acc3.deposit(500);
+
+        acc1.getStatement();
+        acc2.getStatement();
+        acc3.getStatement();
+    }
+}
