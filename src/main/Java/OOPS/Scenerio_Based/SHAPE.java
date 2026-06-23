5320a9aa7139e2ea527588c0f037a337e2fdddf0
Ritika Soni <soniritika172006@gmail.com>
2026-06-23 10:21:34 +0530

    scenerio based

---
+++ src/main/Java/OOPS/Scenerio_Based/SHAPE.java
@@ -0,0 +1,88 @@
+abstract class Shape {
+
+    public abstract double area();
+
+    public abstract double perimeter();
+}
+
+class Circle extends Shape {
+
+    private final double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    public double perimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+class Rectangle extends Shape {
+
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    public double area() {
+        return length * width;
+    }
+
+    public double perimeter() {
+        return 2 * (length + width);
+    }
+}
+
+class Triangle extends Shape {
+
+    private double a;
+    private double b;
+    private double c;
+
+    public Triangle(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double perimeter() {
+        return a + b + c;
+    }
+
+    public double area() {
+        double s = perimeter() / 2;
+        return Math.sqrt(s * (s - a) * (s - b) * (s - c));
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Shape[] shapes = new Shape[3];
+
+        shapes[0] = new Circle(5);
+        shapes[1] = new Rectangle(4, 6);
+        shapes[2] = new Triangle(3, 4, 5);
+
+        System.out.println("Shape Report");
+
+        for (Shape shape : shapes) {
+            System.out.println("Shape: " + shape.getClass().getSimpleName());
+            System.out.println("Area: " + shape.area());
+            System.out.println("Perimeter: " + shape.perimeter());
+            System.out.println();
+        }
+    }
+}
