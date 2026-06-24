3f009ca36a7a068cfb2a92c33269b59963bed0a4
Ritika Soni <soniritika172006@gmail.com>
2026-06-24 09:57:13 +0530

    oops

---
+++ src/main/Java/OOPS/gcr_codebase/Object_Oriented_Programming/CLASS_AND_OBJ/Employee.java
@@ -0,0 +1,30 @@
+package main.Java.OOPS.gcr_codebase.Object_Oriented_Programming.CLASS_AND_OBJ;
+
+class Employ {
+
+    String name;
+    int id;
+    double salary;
+
+    Employ(String name, int id, double salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+
+    void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Salary: " + salary);
+    }
+}
+
+public class Employee {
+
+    public static void main(String[] args) {
+
+        Employ emp1 = new Employ("Ritika", 101, 50000);
+
+        emp1.displayDetails();
+    }
+}
